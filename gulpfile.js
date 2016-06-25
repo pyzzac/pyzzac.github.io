8e7deee258700b5bc3ae00b6dba145bad5b01842
pyzzac <colin@ppadv.com>
2016-06-25 18:48:04 -0400

    Minifying CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 var gulp = require('gulp');
-// Requires the gulp-sass plugin
 var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+
 gulp.task('sass', function(){
   return gulp.src('app/scss/styles.scss')
-    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(sass())
     .pipe(gulp.dest('app/css'))
 });
+
+gulp.task('minify-css', function(){
+  return gulp.src('app/css/styles.css')
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('css'))
+});
+
 gulp.task('watch', function(){
-  gulp.watch('app/scss/**/*.scss', ['sass']); 
-  // Other watchers
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/css/styles.css', ['minify-css']); 
 })
