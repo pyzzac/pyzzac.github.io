436c4084a9a264430fd717b1d7760719a177163e
pyzzac <colin@ppadv.com>
2016-06-26 17:18:06 -0400

    Nav Animations

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-minify-css');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
 
 gulp.task('sass', function(){
   return gulp.src('app/scss/styles.scss')
@@ -18,3 +21,11 @@ gulp.task('watch', function(){
   gulp.watch('app/scss/**/*.scss', ['sass']);
   gulp.watch('app/css/styles.css', ['minify-css']); 
 })
+
+gulp.task('useref', function(){
+  return gulp.src('*.html')
+    .pipe(useref())
+    .pipe(gulp.dest(''))
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('js'))
+});
