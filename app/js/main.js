9dfb74ea7e6dd6a562bce8d5f210f07424ebf60a
pyzzac <colin@ppadv.com>
2016-06-26 12:51:51 -0400

    Scrapping Parallax

--- app/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $(window).scroll(function(){
-    $('*[class^="grumpy-banner"]').each(function(r){
-      var pos = $(this).offset().top;
-      var scrolled = $(window).scrollTop();
-        $('*[class^="grumpy-banner"]').css('top', -(scrolled * 0.5) + 'px');     
-      });
-  });
-});
